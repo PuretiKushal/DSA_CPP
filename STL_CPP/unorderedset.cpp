dc1919985f9dbf23d5c515c17bff1f3ac3f05b8e
Kushal <puretikushal7@gmail.com>
2026-07-05 20:07:59 +0530

    Learn STL unordered_set

---
+++ STL_CPP/unorderedset.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+void explainUnorderedSet()
+{
+    //same like set but not ordered
+    //all operations are O(1),so, better than sets
+    //lower_bound & upper_bound does not work
+    unordered_set<int> u;
+
+
+}
+int main()
+{
+
+}
