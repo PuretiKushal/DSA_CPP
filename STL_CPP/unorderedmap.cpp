b133d57991f01fea06df785dcaac6bc022efac83
Kushal <puretikushal7@gmail.com>
2026-07-05 20:08:18 +0530

    Learn STL unordered_map

---
+++ STL_CPP/unorderedmap.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+void explainUnorderedMap()
+{
+    //doesn't sort, unique key
+    //all operations are O(1)
+    unordered_map<int,int> um;
+
+
+}
+int main()
+{
+    explainUnorderedMap();
+}
